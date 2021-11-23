6c9c9383b96ed2097364ba5fae8dc85208d073eb
NouhaJ <94851132+NouhaJ@users.noreply.github.com>
2021-11-23 11:59:47 +0100

    Update TestRunner.java

--- src/test/java/com/ExpertTestBDD/TestRunner.java
+++ src/test/java/com/ExpertTestBDD/TestRunner.java
@@ -17,7 +17,7 @@ import cucumber.api.junit.Cucumber;
 		plugin = { "pretty", "html:target/cucumber-html-report", "json:json/cucumber.json",
 				
 				"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/Expertest.html" }, 
-		tags = {(" @accueil") },
+		tags = {(" @accueil,@Nos-Promotions, @Header, @quizzpage , @Eboutqiue,@Telechargement, @Actualite, @connexion, @registration , @apropos ") },
 
 					
 		snippets = SnippetType.CAMELCASE, monochrome = true)
